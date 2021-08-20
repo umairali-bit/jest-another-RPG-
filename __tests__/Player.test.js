3097cf391af1afa465f3272517587b97393e8f8d
Umair Ali <umairmamoor@gmail.com>
2021-08-19 20:17:34 -0400

    added inventory execption

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,9 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player ('Dave');
@@ -7,4 +12,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 })
